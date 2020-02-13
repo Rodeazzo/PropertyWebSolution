17e74b29c9dbdeb1b1226b65ca1dcf52529e6d42
Roderick Azzopardi <Roderick.Azzopardi.b43256@mcast.edu.mt>
2020-02-13 15:17:11 +0100

    Add project files.

---
+++ PropertyWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PropertyWebApplication.Startup))]
+namespace PropertyWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
